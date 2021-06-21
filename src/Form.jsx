af2089e694a267af7550edd561f4905d79a175bf
sabanr <sabanr@users.noreply.github.com>
2021-06-21 20:28:38 -0300

    form is a controlled component.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
 class Form extends React.Component {
-    handleSubmit = () => {
-
+    state = {
+        userName: ''
+    };
+    
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.userName);
     };
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <input type="text" placeholder="GitHub username"/>
+                <input 
+                    type="text" 
+                    placeholder="GitHub username" 
+                    value={this.state.userName}
+                    onChange={event => this.setState({userName: event.target.value})}
+                    required/>
                 <button>Add card</button>
             </form>
         );
     }
-};
+}
 
 export default Form;
