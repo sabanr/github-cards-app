7b82c829137ceafff83c722a87d30c2af6bbffdf
sabanr <sabanr@users.noreply.github.com>
2021-06-21 23:29:15 -0300

    no longer use test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
+// GitHub username: gaearon, sophiebits, sebmarkbage, bvaughn, tj 
+
 import React from 'react';
 import CardList from './CardList.jsx'
 import Form from './Form.jsx'
 import './App.css';
 
-const testData = [
-  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-];
-
 class App extends React.Component {
   
   state = {
-    profiles: testData
+    profiles: []
   }
   
   addNewProfile = (profileData) => {
